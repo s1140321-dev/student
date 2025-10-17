b13da1f18766a2f3121e2ae37de318be663371a8
s1140321-dev <s1140321@dmhs.kh.edu.tw>
2025-10-17 16:05:15 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int age = 69;
+    double height = 180.5;
+    std::string name = "Bruce";
+    bool is_pass = true;
+    std::cout << "Name: 李昀叡" << std::endl;
+
+    return 0;
+}
