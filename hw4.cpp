3e2131d6ae0f5e72f1e63bc73ae63b148356b6e8
s1140321-dev <s1140321@dmhs.kh.edu.tw>
2025-11-07 16:01:18 +0800

    Add files via upload

---
+++ hw4.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main()
+{
+    int x1 = 5 + 3;
+    int x2 = 5 - 3;
+    int x3 = 5 * 3;
+    int x4 = 5 / 3;
+    double x5 = 5 / 3.0;
+    int x6 = 5 % 3;
+    std::cout << x1 << " " << x2 << " "<< x3 << " "<< x4 << " "<< x5 << " "<< x6 ;
+    return 0;
+}
