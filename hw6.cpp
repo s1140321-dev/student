7d265bf763c2ff09e085cc1dd03682e034cd2d24
s1140321-dev <s1140321@dmhs.kh.edu.tw>
2025-11-21 16:07:41 +0800

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "請輸入你的年齡" << endl;
+    int age;
+    cin >> age;
+    if (age >= 18) {
+        cout << "18歲成年,可以看 pXXXhub 網站" << endl;
+    }
+    if (age < 18) {
+        cout << "未成年,請你好好讀書" << endl;
+    }
+
+    return 0;
+}
